a5c3bce7a3a382e3afd787782aa201d086496bfd
Frank Palmisano <frank_palmisano@asu.edu>
2017-04-26 18:01:55 -0700

    Completed audio functionality
     - Hard coded, needs improvements

--- Beacon Logic/ViewController.swift
+++ Beacon Logic/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate  {
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     var audioPlayer = AVAudioPlayer()
+    var go = true
     
     let beaconManager = ESTBeaconManager()
     let beaconRegion = CLBeaconRegion(
@@ -22,11 +23,20 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Hi I'm Life Jewel", ofType: "m4a")!))
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        } catch {
+            print(error)
+        }
         // Do any additional setup after loading the view, typically from a nib.
         self.beaconManager.delegate = self
         self.beaconManager.requestAlwaysAuthorization()
         self.beaconManager.startRangingBeacons(in: self.beaconRegion)
         self.activityIndicator.startAnimating()
+       
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,59 +45,95 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate  {
     }
     
     func beaconManager(_ manager: Any, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if go == true {
         if let closest = beacons.first {
-            
             let minor = (closest.minor) as! Int
             
-            if (closest.proximity.rawValue == 1) {
-                self.activityIndicator?.stopAnimating()
-                self.activityIndicator?.isHidden = true
-                switch minor {
-                case 42553:
-                    
-                    self.view.backgroundColor = UIColor.purple
-                    self.label.text = "This is a water bottle!"
-                    self.image.isHidden = false
-                    print("Purple");
-                    var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource)
-                    
-                    
-                case 10335:
-                    
-                    self.view.backgroundColor = UIColor.green
-                    self.label.text = "This is a chair!"
-                    self.image.isHidden = false
-                    print("Mint");
-                    
-                case 53328:
-                    
-                    self.view.backgroundColor = UIColor.blue
-                    self.label.text = "This is a purse!"
-                    self.image.isHidden = false
-                    print("ice");
-                    
-                default:
-                    
-                    self.view.backgroundColor = UIColor.lightGray
-                    self.label.text = "You are not immediately close to an object (within 1m)."
-                    self.image.isHidden = true
-                    self.activityIndicator.startAnimating()
-                    self.activityIndicator.isHidden = false
-                    print("error?")
-                    break;
-                    
+            
+                if (closest.proximity.rawValue == 1) {
+                    self.activityIndicator?.stopAnimating()
+                    self.activityIndicator?.isHidden = true
+                    switch minor {
+                        
+                    case 42553:
+                        self.view.backgroundColor = UIColor.purple
+                        self.label.text = "This is a Television!"
+                        self.image.isHidden = false
+                        print("Purple")
+                        playSoundEffect(minor: minor)
+                        go = false
+                        pause(time: 4500)
+                        
+                    case 10335:
+                        self.view.backgroundColor = UIColor.green
+                        self.label.text = "This is a Couch!"
+                        self.image.isHidden = false
+                        print("Mint")
+                        playSoundEffect(minor: minor)
+                        go = false
+                        pause(time: 4500)
+                        
+                    case 53328:
+                        self.view.backgroundColor = UIColor.blue
+                        self.label.text = "This is a Refrigerator!"
+                        self.image.isHidden = false
+                        print("ice");
+                        playSoundEffect(minor: minor)
+                        go = false
+                        pause(time: 4500)
+                        
+                    default:
+                        self.view.backgroundColor = UIColor.lightGray
+                        self.label.text = "You are not immediately close to an object (within 1m)."
+                        self.image.isHidden = true
+                        self.activityIndicator.startAnimating()
+                        self.activityIndicator.isHidden = false
+                        print("error?")
+                        
+                    }
                 }
-                
-            } else {
-                
-                self.view.backgroundColor = UIColor.lightGray
-                self.label.text = "You are not immediately close to an object (within 1m)."
-                self.image.isHidden = true
-                self.activityIndicator.startAnimating()
-                self.activityIndicator.isHidden = false
-                print("None in range")
-                
             }
+        }
+    }
+    
+    func playSoundEffect(minor: Int) {
+        switch minor {
+        case 42553:
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Television", ofType: "m4a")!))
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch {
+                print(error)
+            }
+
+        case 10335:
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "Couch", ofType: "m4a")!))
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch {
+                print(error)
+            }
+            
+        case 53328:
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "FRIDGE", ofType: "m4a")!))
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            } catch {
+                print(error)
+            }
+            
+        default:
+            print("nothing was played")
+        }
+    }
+    
+    func pause(time: Int) {
+        let deadlineTime = DispatchTime.now() + .milliseconds(time)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime) {
+            self.go = true
         }
     }
 }
